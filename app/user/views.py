09909fa277dfc4df7dc751ce82c1548d0f3722e3
amirthnz <amirthn95@gmail.com>
2024-06-21 23:08:47 -0700

    Added User API and authentication with token authorization

---
+++ app/user/views.py
@@ -0,0 +1,51 @@
+"""
+Views for the user API
+"""
+from rest_framework import (
+    generics,
+    authentication,
+    permissions
+)
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        """Look we can override functions of the inherited class"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'custom message': 'Hello dear user'
+        })
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Retrieve and update user instance"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+
+        return self.request.user
